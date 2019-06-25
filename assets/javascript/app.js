583b3f6f4ca0a2d1237bab5bc58489eb24a7476c
Thai Phan <thaiphan90@yahoo.com>
2019-06-24 21:00:21 -0500

    intializing questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,64 @@
 /* ---------- Variabls ---------- */
-var correct = {};
-var incorrect = {};
+var correct = 0;
+var incorrect = 0;
+var timesUp = false;
+
+/* ---------- Questions ---------- */
+var question1 = {
+    q: "???",
+    a: "aa",
+    b: "bb",
+    c: "cc",
+};
+
+var question2 = {
+    q: "???",
+    a: "xx",
+    b: "xx",
+    c: "xx",
+};
+
+var question3 = {
+    q: "???",
+    a: "xx",
+    b: "xx",
+    c: "xx",
+};
+
+var question4 = {
+    q: "???",
+    a: "xx",
+    b: "xx",
+    c: "xx",
+};
+
+var question5 = {
+    q: "???",
+    a: "xx",
+    b: "xx",
+    c: "xx",
+};
+
+
+/* ---------- Functions ---------- */
+function start() {
+    $("#start").click(function() {
+        $("#start").hide();
+        $("#questions").append(question1.q);
+        $("#a").show();
+        $("#b").show();
+        $("#c").show();
+        $("#a").append(question1.a);
+        $("#b").append(question1.b);
+        $("#c").append(question1.c);
+    });
+};
+
+/* ---------- Prcoesses ---------- */
+$(document).ready(function() {
+    
+    $("#a").hide();
+    $("#b").hide();
+    $("#c").hide();
+    start();
+})
